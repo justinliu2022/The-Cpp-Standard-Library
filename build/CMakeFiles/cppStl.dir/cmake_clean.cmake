45ddefb88decca82f98fc929cddbeaa6a7b421ee
justinliu2022 <1066486569@qq.com>
2024-05-20 13:37:33 +0800

    新增程序

---
+++ build/CMakeFiles/cppStl.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppStl.dir/main.cpp.obj"
+  "CMakeFiles/cppStl.dir/main.cpp.obj.d"
+  "cppStl.exe"
+  "cppStl.exe.manifest"
+  "cppStl.pdb"
+  "libcppStl.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppStl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cppStl.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cppStl.
+# This may be replaced when dependencies are built.
